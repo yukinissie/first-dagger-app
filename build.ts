d2d9825e6b24cfc5b8f6cd5422ed4d1d77ba1440
yukinissie <bigtree2490@outlook.jp>
2022-12-24 08:46:23 +0900

    🧱 Update build pipeline

--- build.ts
+++ build.ts
@@ -2,26 +2,32 @@ import Client, { connect } from "@dagger.io/dagger";
 
 // initialize Dagger client
 connect(async (client: Client) => {
+  // Set Node versions against which to test and build
+  const nodeVersions = ["12", "14", "16"];
+
   // get reference to the local project
   const source = client.host().directory(".", { exclude: ["node_modules/"] });
 
-  // get Node image
-  const node = client.container().from("node:16");
+  // for each Node version
+  for (const nodeVersion of nodeVersions) {
+    // get Node image
+    const node = client.container().from(`node:${nodeVersion}`);
 
-  // mount cloned repository into Node image
-  const runner = client
-    .container({ id: node })
-    .withMountedDirectory("/src", source)
-    .withWorkdir("/src")
-    .withExec(["yarn"]);
+    // mount cloned repository into Node image
+    const runner = client
+      .container({ id: node })
+      .withMountedDirectory("/src", source)
+      .withWorkdir("/src")
+      .withExec(["npm", "install"]);
 
-  // run tests
-  await runner.withExec(["yarn", "test", "--watchAll=false"]).exitCode();
+    // run tests
+    await runner.withExec(["npm", "test", "--", "--watchAll=false"]).exitCode();
 
-  // build application
-  // write the build output to the host
-  await runner
-    .withExec(["yarn", "run", "build"])
-    .directory("build/")
-    .export("./build");
+    // build application using specified Node version
+    // write the build output to the host
+    await runner
+      .withExec(["npm", "run", "build"])
+      .directory("build/")
+      .export(`./build-node-${nodeVersion}`);
+  }
 });
